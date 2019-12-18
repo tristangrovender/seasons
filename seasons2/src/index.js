94c2883eb6bfe9ee72a4c99e8a63be553f459f1f
Tristan Grovender <therealtristangrovender@gmail.com>
2019-12-18 10:49:30 -0600

    Moved login from render into helper function

--- seasons2/src/index.js
+++ seasons2/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-import Spinner from './Spinner';
+import Spinner from "./Spinner";
 
 class App extends React.Component {
     state = { lat: null, errorMessage: "" };
@@ -13,8 +13,7 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render!!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -24,6 +23,10 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return <div className="border red">{this.renderContent()}</div>;
     }
 }
 
