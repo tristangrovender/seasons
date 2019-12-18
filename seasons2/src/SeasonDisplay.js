064ea7a0c6a3d3f84b4128dd5bf24dcc2fb78225
Tristan Grovender <therealtristangrovender@gmail.com>
2019-12-18 11:24:57 -0600

    Finishing touches - formatting

--- seasons2/src/SeasonDisplay.js
+++ seasons2/src/SeasonDisplay.js
@@ -1,16 +1,16 @@
-import './SeasonDisplay.css';
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
     summer: {
         text: "Let's hit the beach!",
-        iconName: 'sun'
+        iconName: "sun"
     },
     winter: {
         text: "Burr it's cold!",
-        iconName: 'snowflake'
+        iconName: "snowflake"
     }
-}
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
@@ -22,13 +22,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const { text, iconName } = seasonConfig[season] // { text, iconName }
+    const { text, iconName } = seasonConfig[season]; // { text, iconName }
 
-    return <div className={`season-display ${season}`}>
-    <i className={`icon-left massive ${iconName} icon`} />
-    <h1>{text}</h1>
-    <i className={`icon-right massive ${iconName} icon`} />
-    </div>;
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
